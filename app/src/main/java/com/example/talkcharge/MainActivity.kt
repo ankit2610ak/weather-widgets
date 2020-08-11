e1f44d4791961bcfd127c8f224f825da6735ab0e
Ankit Tiwari (ak) <ankit2610akt@gmail.com>
2020-08-12 02:41:51 +0530

    feat: Get current Location

--- app/src/main/java/com/example/talkcharge/MainActivity.kt
+++ app/src/main/java/com/example/talkcharge/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.talkcharge
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationProviderClient!!.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    // Got last known location. In some rare situations this can be null.
+                    Log.d(TAG, "lat: " + location?.latitude)
+                }
+
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                44
+            )
+        }
+
+
     }
+
 }
