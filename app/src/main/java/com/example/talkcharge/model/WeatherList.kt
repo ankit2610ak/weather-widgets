be9d2e19c176213b599e6c75935eb72868ba2f15
Ankit Tiwari (ak) <ankit2610akt@gmail.com>
2020-08-12 21:32:42 +0530

    Update modal classes

--- app/src/main/java/com/example/talkcharge/model/WeatherList.kt
+++ app/src/main/java/com/example/talkcharge/model/WeatherList.kt
@@ -4,6 +4,7 @@ data class WeatherList(
     var id: Int,
     val main: Main,
     val weather: ArrayList<WeatherCloud>,
+    val wind : Wind,
     val dt_txt: String
 
 )
---
+++ app/src/main/java/com/example/talkcharge/model/Wind.kt
@@ -0,0 +1,7 @@
+package com.example.talkcharge.model
+
+import java.math.BigDecimal
+
+data class Wind(
+    val speed : BigDecimal
+)
